30413ec48420c414dcb2e8b710dc7b41484eb8d8
Bruno Cunha <rocks.bn@gmail.com>
2017-11-04 10:00:30 -0200

    Add função que faz pesquisa por um elemento na árvore.

--- arv_vetor_apont.py
+++ arv_vetor_apont.py
@@ -1,10 +1,35 @@
 
 class ArvVetorApont(object):
     info = None     #informação a ser armazenada
-    filhos = []     #subárvores filhas
+    filhos = None     #subárvores filhas
 
-    def __init__(self, info):
+    def __init__(self, info): #cria uma árvore vazia
         self.info = info
+        self.filhos = []
+
+    #pesquisa info na arvore
+    def estaArvNaria(self, info):
+        if (self.info == info):
+            return True
+        else:
+            #percorre pelos filhos e vê se algum possui o valor
+            for x in self.filhos:
+                if (x.info == info):
+                    return True
+
+            #olha os filhos dos filhos
+            for x in self.filhos:
+                if (x.estaArvNaria(info) == True):
+                    return True
+                
+            return False
+
+
+    #imprme os filhos
+    def listaFilhos(self):
+        for x in self.filhos:
+            print (x.info)
+
 
     def teste (self):
         print ("oi " + self.info)
