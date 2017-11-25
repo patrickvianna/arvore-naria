b531f1bf632e1becf3878eeeb1b082822c68fcd5
Unknown <patrickviannapblv@gmail.com>
2017-11-24 22:09:21 -0200

    arv_filho_esquerda_irmao_direita  --- Menu Pronto

--- arv_filho_esquerda_irmao_direita.py
+++ arv_filho_esquerda_irmao_direita.py
@@ -1,36 +1,43 @@
-
 class ArvFilhoEsqIrmaoDir:
-    #region atributos
+    # region atributos
     info = None
     filhoEsq = None
     irmaoDir = None
-    #endregion
+
+    # endregion
 
     # Em todas as funções, self é a raiz
 
+    #region [[  CONSTRUTOR  ]]
     def __init__(self):
         self.filhoEsq = None
         self.irmaoDir = None
         self.info = None
-
+    #endregion
+    # region [[  CRIA  ]]
     def criarArvoreNaria(self, info):
         self.filhoEsq = None
         self.irmaoDir = None
         self.info = info
 
+    # endregion
+    # region [[  INSERE  ]]
     def inserirArvoreNaria(self, noPai, novaChave):
         pai = self.buscarNo(noPai)
         if pai is None: return False
         filho = ArvFilhoEsqIrmaoDir()
         filho.criarArvoreNaria(novaChave)
         p = pai.filhoEsq
-        if(p == None): pai.filhoEsq = filho
+        if (p == None):
+            pai.filhoEsq = filho
         else:
-            while(p.irmaoDir):
+            while (p.irmaoDir):
                 p = p.irmaoDir
             p.irmaoDir = filho
         return True
 
+    # endregion
+    # region [[  BUSCAR  ]]
     def buscarNo(self, no):
         if self is None: return
         if self.info == no: return self
@@ -41,38 +48,45 @@ class ArvFilhoEsqIrmaoDir:
             p = p.irmaoDir
         return None
 
+    # endregion
+    #region [[  EXIBIR  ]]
     def exibirArvoreNaria(self):
-       if self is None: return
-       print (str(self.info) + "(", end="")
-       p = self.filhoEsq
-       while p:
+        if self is None: return False
+        print(str(self.info) + "(", end="")
+        p = self.filhoEsq
+        while p:
             p.exibirArvoreNaria()
             p = p.irmaoDir
+        print(") ", end="")
+        return True
 
-       print(") ", end="")
 
+    #endregion
+    # region [[  EXCLUIR NO  ]]
     def excluirNo(self, no):
-        if self is None: return
+        if self is None: return False
         excluido = self.buscarNo(no)
-        if excluido is None: return False
+        if excluido is None:
+            return False
         else:
             excluido = excluido.excluirFilhosNo()
+            return True
 
         return
 
+    #endregion
+
+    #region [[  EXCLUIR FILHOS  ]]
     def excluirFilhosNo(self):
         if (self.filhoEsq is not None):
-            print ("Pai:" + str(self.info) + " - Filho:" + str(self.filhoEsq.info))
+            print("Pai:" + str(self.info) + " - Filho:" + str(self.filhoEsq.info))
             self.filhoEsq = self.filhoEsq.excluirFilhosNo()
 
         if (self.irmaoDir is not None):
-            print ("Pai:" + str(self.info) + " - Irmao:" + str(self.irmaoDir.info))
+            print("Pai:" + str(self.info) + " - Irmao:" + str(self.irmaoDir.info))
             self.v = self.irmaoDir.excluirFilhosNo()
 
         return None
 
 
-
-
-
-
+    #endregion
--- main2.py
+++ main2.py
@@ -1,27 +1,7 @@
 from arv_filho_esquerda_irmao_direita import ArvFilhoEsqIrmaoDir
+import os
 def cls():
     print("\n" * 100)
-
-
-'''
-r = ArvFilhoEsqIrmaoDir()
-
-r.criarArvoreNaria(0)
-r.inserirArvoreNaria(0, 1)
-r.inserirArvoreNaria(0, 2)
-r.inserirArvoreNaria(0, 3)
-r.inserirArvoreNaria(3, 4)
-r.inserirArvoreNaria(4, 5)
-r.inserirArvoreNaria(3, 6)
-r.inserirArvoreNaria(6, 7)
-r.inserirArvoreNaria(6, 8)
-r.inserirArvoreNaria(7, 9)
-r.exibirArvoreNaria()
-print("")
-r.excluirNo(6)
-print("")
-r.exibirArvoreNaria()
-'''
 
 print( "############################################################")
 print("MENU :::: ARVORE N-ARIA :::: FILHO ESQUERDA - IRMAO DIREITA \n")
@@ -38,15 +18,51 @@ controle = 9
 while(controle != 0 ):
     print( "############################################################")
     print("MENU :::: ARVORE N-ARIA :::: FILHO ESQUERDA - IRMAO DIREITA \n")
-    print("1 - INSEROR NO")
+    print("1 - INSERIR NO")
     print("2 - EXCLUIR NO")
+    print("3 - EXIBIR ARVORE ")
     print("0 - SAIR")
     print( "############################################################\n\n")
 
     controle = int(input("RESPOSTA:: "))
 
+    #<editor-fold desc="[[  INSERIR  ]]">
     if controle == 1:
-        print("INSERIR EM QUAL NÓ?")
+        no = int(input("INSERIR EM QUAL NÓ?  "))
+        info = int(input("QUAL VALOR?  "))
+        retorno = raiz.inserirArvoreNaria(no, info)
+        if retorno:
+            cls()
+            print("INSERIDO COM SUCESSO!!! \n")
+            os.system('pause')
+            cls()
+        else:
+            cls()
+            print("NAO EXISTE ESSE NO")
+            os.system('pause')
+            cls()
+
+    if controle == 2:
+        no = int(input("EXCLUIR QUAL NO?  "))
+        resultado = raiz.excluirNo(no)
+        if resultado:
+            cls()
+            print("EXCLUIDO COM SUCESSO!!! \n")
+            os.system('pause')
+            cls()
+        else:
+            cls()
+            print("NAO FOI POSSIVEL EXCLUIR O NO")
+            os.system('pause')
+            cls()
+
+    if controle == 3:
+        cls()
+        print("ARVORE:: ")
+        resultado = raiz.exibirArvoreNaria()
+        print("")
+        os.system('pause')
+        cls()
 else:
     print("FALOU!")
 
